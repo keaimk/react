d49ea4afb61860e2cb378794f2bb8a10582ac5f3
keaimk <fly1043@naver.com>
2024-11-06 09:50:37 +0900

    fragment

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App1 from "./practice/App1.jsx";
+import App5 from "./practice/App5.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App1 />
+    <App5 />
   </StrictMode>,
 );
