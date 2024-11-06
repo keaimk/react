723c644cb8ef443f392a81044c9d4ace1acc3b46
keaimk <fly1043@naver.com>
2024-11-06 11:24:14 +0900

    {} 내 연산자 사용

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App6 from "./practice/App6.jsx";
+import MyApp8 from "./apps/MyApp8.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App6 />
+    <MyApp8 />
   </StrictMode>,
 );
