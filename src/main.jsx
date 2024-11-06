2b886943ce08f890c7c5b8b4f5b068269b6e4555
keaimk <fly1043@naver.com>
2024-11-06 16:36:31 +0900

    chakra ui 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MyApp23 from "./apps/MyApp23.jsx";
+import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp23 />
+    <Provider>
+      <MyApp23 />
+    </Provider>
   </StrictMode>,
 );
