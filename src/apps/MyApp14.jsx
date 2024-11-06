0efac487027859c7746488675c037d57a971603d
keaimk <fly1043@naver.com>
2024-11-06 14:34:26 +0900

    export, import

---
+++ src/apps/MyApp14.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import zxc, { a, b, f as fa } from "./MyApp13.jsx";
+import { d, e, f as fb } from "./MyApp15.jsx";
+
+function MyApp14(props) {
+  return (
+    <div>
+      <h1>my app14</h1>
+      <h1>{a}</h1>
+      <h1>{b}</h1>
+      <h1>{zxc}</h1>
+      <h1>{d}</h1>
+      <h1>{e}</h1>
+      <h1>
+        {fa}, {fb}
+      </h1>
+    </div>
+  );
+}
+
+export default MyApp14;
+//
+// import React from "react";
+// import zx, { a, b, f as fa } from "./MyApp13.jsx";
+// import { d, e as ea, f as fb } from "./MyApp15.jsx";
+//
+// function MyApp14(props) {
+//   return (
+//     <div>
+//       <h1>myapp 14</h1>
+//       <h1>{a}</h1>
+//       <h1>{b}</h1>
+//       <h1>{zx}</h1>
+//       <h1>{d}</h1>
+//       <h1>{ea}</h1>
+//       <h1>
+//         {fa}, {fb}
+//       </h1>
+//     </div>
+//   );
+// }
+//
+// export default MyApp14;
