9704baa4a945d691d5ab46933c14c66cf1b29b3d
keaimk <fly1043@naver.com>
2024-11-06 09:00:36 +0900

    react 맛보기

---
+++ src/apps/MyApp1.jsx
@@ -0,0 +1,16 @@
+function MyApp1() {
+  return (
+    <div>
+      첫 번째 app
+      <p>Lorem.</p>
+      <ul>
+        <li>Lorem.</li>
+        <li>Sapiente?</li>
+        <li>Dolor.</li>
+      </ul>
+      <h3>Lorem.</h3>
+    </div>
+  );
+}
+
+export default MyApp1;
