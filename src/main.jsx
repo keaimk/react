84e2c448a9e7ac066b66cf7739c7c64a7cee50bc
keaimk <fly1043@naver.com>
2024-11-07 09:50:18 +0900

    input field

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp26 from "./apps/MyApp26.jsx";
+import MyApp28 from "./apps/MyApp28.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp26 />
+      <MyApp28 />
     </Provider>
   </StrictMode>,
 );
