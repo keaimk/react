67fde209b87057023c6f20994dbebceebd69228e
keaimk <fly1043@naver.com>
2024-11-08 15:47:15 +0900

    context로 prop drilling 없이 데이터 전달

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App48 from "./practice/App48.jsx";
+import App49 from "./practice/App49.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <App48 />
+    <App49 />
     <Toaster />
   </Provider>,
   // </StrictMode>,
---
+++ src/practice/App49.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState } from "react";
+import { Button } from "../components/ui/button.jsx";
+import { Box } from "@chakra-ui/react";
+
+const CountContext = createContext("");
+function App49(props) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <Button onClick={() => setCount(count + 1)}>count {count}</Button>
+      <Child1 count={count} />
+    </div>
+  );
+}
+function Child1() {
+  const count = useContext(CountContext);
+  return <Child2 />;
+}
+
+function Child2({ count }) {
+  return <Box>{count}</Box>;
+}
+
+export default App49;
