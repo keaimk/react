57491571efe4bd3ea8b2091bcc9e7c20128d6c18
keaimk <fly1043@naver.com>
2024-11-08 15:47:19 +0900

    context로 prop drilling 없이 데이터 전달

---
+++ src/practice/App46.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import {Button} from "../components/ui/button.jsx";
+import {Box} from "@chakra-ui/react";
+
+function App46(props) {
+  return <div></div>;
+}
+
+export default App46;
+
+function Comp({ count }) {
+  return (
+    <div>
+      <Box bg={"red.300"}>{count}</Box>
+    </div>
+  );
+}
+function MyApp46(props) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Button onClick={() => setCount(count + 1)}>count: {count}</Button>
+      <Comp count={count} />
+    </div>
+  );
+}
+
+export default MyApp46;
