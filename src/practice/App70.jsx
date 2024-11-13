00198a75a31e3d9e2460f9701bee6824f70ed942
keaimk <fly1043@naver.com>
2024-11-13 12:15:48 +0900

    파일 형식 지정

--- src/practice/App70.jsx
+++ src/practice/App70.jsx
@@ -6,22 +6,23 @@ function App70(props) {
   const [files, setFiles] = useState([]);
 
   const handleClick = () => {
-    axios.postForm("/api/main8/sub4", {
-      name: name,
-      files: files,
-    });
+    axios.postForm("/api/main8/sub4", { name: name, attached: files });
   };
 
   return (
     <div>
       <input
-        placeholder={"이름"}
         type={"text"}
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <br />
-      <input type="file" multiple onChange={(e) => setFiles(e.target.files)} />
+      <input
+        accept={"image/*"}
+        type="file"
+        multiple
+        onChange={(e) => setFiles(e.target.files)}
+      />
       <br />
       <button onClick={handleClick}>전송</button>
     </div>
